be052aa3317dd50522e9a1dc7eca15dbebc050a3
Alexey Smirnov <xlwknx@gmail.com>
2021-03-20 16:38:56 +0200

    fix css build and add link to header

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -1,23 +1,25 @@
-var PrismicDOM = require('prismic-dom');
-
 const linkResolver = (doc) => {
     if (doc.link_type === "Web") {
-      return `${doc.url}`
+        return `${doc.url}`
     }
     // URL for a category type
-    if (doc.type === 'homepage') {
-      //console.log("homepage resolved")
-      return `/`
+    if (doc.type === "homepage") {
+        //console.log("homepage resolved")
+        return `/`
     }
     // URL for a page type
-    if (doc.type === 'page') {
-      //console.log(`/blog/${doc.uid}`)
-      return `/blog/${doc.uid}`
+    if (doc.type === "page") {
+        //console.log(`/blog/${doc.uid}`)
+        return `/blog/${doc.uid}`
     }
-  
+
+    if (doc.type === "faq_page") {
+        return `/faq`
+    }
+
     // Backup for all other types
     //console.log("unknown type resolved")
-    return '/'
-  }
-  
-  module.exports = linkResolver
+    return "/"
+}
+
+module.exports = linkResolver
